30443fed94aeba78b2f3f92a9a2c8050f3000e58
Mateusz Stępień <m.stepien29@gmail.com>
2019-06-01 22:56:11 +0200

    MS: Add Lab 12, Lab06 and edit Lab10

---
+++ Lab12/Lab12/Assets/Script/headTurn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class headTurn : MonoBehaviour
+{
+
+    private Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetKeyDown("1"))
+        {
+            anim.SetTrigger("HeadTurn");
+        }
+
+    }
+}
