0302d3c469ad130cb6eed51b3dbe29db5123d1f4
Mateusz Stępień <m.stepien29@gmail.com>
2019-06-04 01:01:24 +0200

    MS: edit Lab3

--- Lab03/Lab_03/Assets/Script/Cannon.cs
+++ Lab03/Lab_03/Assets/Script/Cannon.cs
@@ -1,41 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Cannon : MonoBehaviour
 {
     public GameObject CannonBall;
     Rigidbody CannonBallRB;
+
+    public Slider slider;
+
     public Transform ShootPoint;
+
     public float firepower;
+    public float force;
+
+    private bool firebool = false;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-     
-        if(Input.GetMouseButtonDown(0))
+
+        if (Input.GetMouseButton(0))
         {
-            Fire();
-        }
+            force = force + 15;
+
+            if (force > 5000)
+            {
+                force = 5000;
+            }
+
             
+        }
 
-     }
+        if (Input.GetMouseButtonUp(0))
+        {
+            CannonBall.GetComponent<ConstantForce>().force = transform.forward * force;
+            GameObject CannonBallCopy = Instantiate(CannonBall, ShootPoint.position, ShootPoint.rotation) as GameObject;
+            force = 0;
+            Destroy(CannonBallCopy, 5f);
 
-    public void Fire()
-    {
 
-        float ShootPower = firepower * 1000;
-        GameObject CannonBallCopy = Instantiate(CannonBall, ShootPoint.position, ShootPoint.rotation) as GameObject;
-        Destroy(CannonBallCopy, 5f);
-        CannonBallRB = CannonBallCopy.GetComponent<Rigidbody>();
-        CannonBallRB.AddForce(transform.forward * ShootPower);
-   
+        }
+
+        float progressForce = Mathf.Clamp01(force / 5000);
+        slider.value = progressForce;
+
     }
+
+
+
+
 }
